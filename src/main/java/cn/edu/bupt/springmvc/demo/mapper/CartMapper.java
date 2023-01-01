f8faddee72bb3d6fac76093d8bc71ded133138c9
po13on <h13590641542@163.com>
2023-01-01 20:57:13 +0800

    增加cart类mapper方法

--- src/main/java/cn/edu/bupt/springmvc/demo/mapper/CartMapper.java
+++ src/main/java/cn/edu/bupt/springmvc/demo/mapper/CartMapper.java
@@ -4,4 +4,11 @@ import cn.edu.bupt.springmvc.demo.entities.Cart;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 public interface CartMapper extends BaseMapper<Cart> {
+    int insert(Cart cart);//加入商品
+
+    int updateNumByid(Long id,Long num);//更新数量
+
+    int findByidanduser(Long food_id,String username);//根据用户和食物查询
+
+
 }
