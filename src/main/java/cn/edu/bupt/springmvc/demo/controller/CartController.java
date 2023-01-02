761bc154de100ed78e9aded2984296aa19061fca
felixlao <FelixLao.CW@gmail.com>
2023-01-02 16:39:40 +0800

    edit cart controller

--- src/main/java/cn/edu/bupt/springmvc/demo/controller/CartController.java
+++ src/main/java/cn/edu/bupt/springmvc/demo/controller/CartController.java
@@ -5,6 +5,8 @@ import cn.edu.bupt.springmvc.demo.util.JsonRsult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+
 
 import javax.servlet.http.HttpSession;
 
@@ -13,11 +15,13 @@ import static javax.security.auth.callback.ConfirmationCallback.OK;
 @RestController
 @RequestMapping("carts")
 public class CartController {
+
     @Autowired
     private ICartService cartService;
 
     @RequestMapping("add_to_cart")
-    public JsonRsult<Void> addToCart(long num, long food_id, HttpSession session){
-        cartService.addtoCart(food_id,username,num);
+    public JsonRsult<Void> addToCart(long num, long food_id, HttpSession session, Authentication authentication) {
+        cartService.addtoCart(food_id, authentication.getAuthorities().toString(), num);
         return new JsonRsult<>(OK);
+    }
 }
